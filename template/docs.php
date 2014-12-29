2c53880fc253afb634342495a7db5705c1a8e6cb
maulana yusuf <im.idiiot@gmail.com>
2014-12-30 00:13:50 +0700

    fix buugs

--- template/docs.php
+++ template/docs.php
@@ -6,13 +6,7 @@ require_once "../config/conf_file.php";
 
 class PDF extends FPDF
 {
-	// $params = array(
-	//      'host' => host, 
-	//      'user' => user, 
-	//      'password' => password, 
-	//      'database' => database
-	// );
-// Load data
+
 function LoadData()
 {
 
@@ -28,51 +22,13 @@ function LoadData()
 	return $rows;
 }
 
-// Simple table
-function BasicTable($header, $data)
-{
-	// Header
-	foreach($header as $col)
-		$this->Cell(40,7,$col,1);
-	$this->Ln();
-	// Data
-	foreach($data as $row)
-	{
-		foreach($row as $col)
-			$this->Cell(40,6,$col,1);
-		$this->Ln();
-	}
-}
-
-// Better table
-function ImprovedTable($header, $data)
-{
-	// Column widths
-	$w = array(40, 35, 40, 45);
-	// Header
-	for($i=0;$i<count($header);$i++)
-		$this->Cell($w[$i],7,$header[$i],1,0,'C');
-	$this->Ln();
-	// Data
-	foreach($data as $row)
-	{
-		$this->Cell($w[0],6,$row[0],'LR');
-		$this->Cell($w[1],6,$row[1],'LR');
-		$this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
-		$this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
-		$this->Ln();
-	}
-	// Closing line
-	$this->Cell(array_sum($w),0,'','T');
-}
-
 // Colored table
 function FancyTable($header, $data)
 {
 	// Colors, line width and bold font
-	$this->SetFillColor(255,0,0);
+	$this->SetFillColor(80,80,80);
 	$this->SetTextColor(255);
-	$this->SetDrawColor(128,0,0);
+	$this->SetDrawColor(0,0,0);
 	$this->SetLineWidth(.3);
 	$this->SetFont('','B');
 	// Header
@@ -87,6 +43,7 @@ function FancyTable($header, $data)
 	// Data
 	$fill = false;
 	$i = 1;
+	$sum = 0;
 	foreach($data as $row)
 	{
 		$this->Cell($w[0],6,$i,'LR',0,'C',$fill);
@@ -96,8 +53,10 @@ function FancyTable($header, $data)
 		$this->Ln();
 		$fill = !$fill;
 		$i++;
+		$sum += $row->qty;
 	}
 	// Closing line
+	// $this->Cell($w,6,$i,'LR',0,'C',$fill);
 	$this->Cell(array_sum($w),0,'','T');
 }
 }
@@ -108,10 +67,6 @@ $header = array('No', 'Kode Barang', 'Nama Barang', 'Jumlah');
 // Data loading
 $data = $pdf->LoadData();
 $pdf->SetFont('Arial','',14);
-// $pdf->AddPage();
-// $pdf->BasicTable($header,$data);
-// $pdf->AddPage();
-// $pdf->ImprovedTable($header,$data);
 $pdf->AddPage();
 $pdf->FancyTable($header,$data);
 $pdf->Output();
